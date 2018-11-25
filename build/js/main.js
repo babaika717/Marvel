9eaed3c3d522c304fd0d6bcc3b4e8489897026d5
Sergey <serega2033@gmail.com>
2018-11-25 19:47:25 +0200

    + 1 block gallary

--- build/js/main.js
+++ build/js/main.js
@@ -3,5 +3,9 @@
 
 $(document).ready(function () {
 
+    $('.work-menu-active').on('click', function (e) {
+        e.preventDefault();
+        $('.work-menu li').animate({width: 'toggle'});
+    })
 
 });
