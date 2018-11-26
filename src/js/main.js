42fd06c0d481fae30bda06c6b5a07e54515fffa7
Sergey <serega2033@gmail.com>
2018-11-26 23:55:03 +0200

    + 1 block features

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,6 @@
 
 $(document).ready(function () {
 
-    $('.work-menu-active').on('click', function (e) {
-        e.preventDefault();
-        $('.work-menu li').animate({width: 'toggle'});
-    })
+    $(".work-gallary").lightGallery();
 
 });
