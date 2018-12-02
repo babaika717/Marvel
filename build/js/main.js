f992714195bf642faf9a91b745da90368b175e16
Sergey <serega2033@gmail.com>
2018-12-02 16:08:50 +0200

    Desktop - done

--- build/js/main.js
+++ build/js/main.js
@@ -5,4 +5,17 @@ $(document).ready(function () {
 
     $(".work-gallary").lightGallery();
 
+    $('.locate-title h1').on('click', function () {
+        $(this).parent().parent().parent().children('iframe').slideToggle(250);
+    });
+
+
+    // var selectedClass = jQuery(this).attr("data-rel");
+    // jQuery("#portfolio .inner a").fadeTo(100, 0.1);
+    // jQuery("#portfolio .inner a").not("." + selectedClass).fadeOut().removeClass('scale-anm');
+    // setTimeout(function () {
+    //     jQuery("." + selectedClass).fadeIn().addClass('scale-anm');
+    //     jQuery("#portfolio .inner a").fadeTo(300, 1);
+    // }, 300);
+    //
 });
